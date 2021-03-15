1544c99df1a89b833e321234e2e97c431b49699f
ismaila turner <64342151+ismaila-turner@users.noreply.github.com>
2021-03-15 01:56:56 +0000

    Add files via upload

---
+++ AmunusWork.java
@@ -0,0 +1,237 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amunus.work;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alieu Secka
+ */
+public class AmunusWork {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main (String[] args) {
+        // TODO code application logic here
+       
+      
+  //double dalasis, GFA=1,amount;
+      
+  //System.out.println("please enter your amount in CFA");
+  //Scanner input=new Scanner(System.in);
+ //GFA = input.nextDouble();
+ //amount= GFA/11.355;
+       
+   // System.out.println("your amount in gambia dalasis is "+amount);
+  
+  
+ 
+  
+  
+    
+  //System.out.println("please enter your frist number");
+  Scanner input=new Scanner(System.in);
+ // int first=input.nextInt();
+  
+ // System.out.println("please enter your second number");
+  
+ //int second=input.nextInt();
+  
+ //// System.out.println("please enter your thrid number");
+ // int third=input.nextInt();
+  
+ // int max= Math.max( first,second);
+ // int finalMax=Math.max(max, third);
+ // System.out.println(" the max is "+finalMax);
+ 
+ 
+ 
+ 
+ 
+ // int english, maths,biology,average;
+ // System.out.println("enter your english score");
+ // english=input.nextInt();
+  
+ // System.out.println("enter your maths score");
+ // maths=input.nextInt();
+  
+ // System.out.println("enter your biology score");
+ // biology=input.nextInt();
+  
+ // average=english+biology+maths;
+   //     int totalAverage = average/3;
+     //   
+       // if(totalAverage<60){
+       // System.out.println("your total average is " +totalAverage+ " you fail " );
+       // }
+       // else{
+       // System.out.println("your total average is " +totalAverage+ " you pass " );
+        //}
+        
+        
+     //   for(int i=10; i>=0; i--){
+  //System.out.println(i);
+    //   }
+        
+        
+//String salesPerson;
+  //     double baseSalary,totalSales,commisionRate,payment,newPayment;
+  // int daysAbsent;
+        
+    //   System.out.println("enter sales person name" );
+      //salesPerson =input.nextLine();
+       
+       // System.out.println("enter basicSalary" );
+      //  baseSalary =input.nextDouble();
+       
+        //System.out.println("enter total sales " );
+       // totalSales =input.nextDouble();
+       
+       // System.out.println("enter commision rate " );
+       // commisionRate =input.nextDouble();
+      
+      // System.out.println("enter days absent" );
+      //  daysAbsent =input.nextInt();
+      // payment= baseSalary+totalSales+commisionRate;
+        //newPayment=(payment-daysAbsent)*100;
+          //      System.out.println(newPayment );
+          
+          
+          
+//          
+//     System.out.println("enter your name");
+//  String name=input.nextLine();     
+////          
+//   System.out.println("enter subject 1 score");
+//   double subject1=input.nextDouble();
+////    
+//    System.out.println("enter subject 2 score");   double subject2=input.nextDouble();
+//
+//           System.out.println("enter subject 3 score");
+//    double subject3=input.nextDouble();
+//    
+//           System.out.println("enter subject 4 score");
+//    double subject4=input.nextDouble();
+//    
+//     System.out.println("enter subject 5 score");
+//    double subject5=input.nextDouble();
+//    
+//    double SubjectTotal=subject1+subject2+subject3+subject4+subject5;
+//    double subjectAverage=SubjectTotal/5;
+//    
+//    if( subjectAverage>50){
+//       System.out.println( name +" That's good you pass your average is "+subjectAverage);
+//    
+//    
+//    }else{
+//     System.out.println(name+" you fail ");
+//    }
+//int count=0;
+//    while( count<=3  ){
+// System.out.println("WE ARE HERE TO HELP YOU COVERT YOUR MONEY  ");
+//    System.out.println("............................................ ");
+//    
+// 
+// System.out.println("enter 1 for dollar to dalasis and 2 from dallasis to dollars");
+//    int option=input.nextInt();
+//    
+//    System.out.println("enter your amount");
+//    double amount=input.nextDouble();
+//    
+//    switch(option){
+//   
+//        
+//        case 1:
+// double  dalasis=   amount*50;      
+//    System.out.println("your amount from dollars to dalaisi is D" +dalasis);
+//    System.out.println("....." )
+//             ;
+//    break;
+//    
+//        case 2:
+//    double dollars=amount/50;
+//     System.out.println("your amount from dalasis to dollar is $" +dollars )
+//             ;
+//     System.out.println(".......")
+//             ;
+//    break;
+//        default:
+//        System.out.println(" Error !!!!! enter 1 or 2 only as your first input"); 
+//        
+//      System.out.println(" ...."); 
+//    }
+//    
+//    
+//count++;
+//int pev=100;
+//for(int x=5; x>=-5; x--){
+//pev=pev+5;
+//}
+//System.out.print(pev);
+//System.out.print("3+4");
+//
+
+double[] items={43,5,6,78,2,4,6,6,8,8,2,4,6,8,9,12};
+double total=0;
+for(int i=0; i<items.length; i++){
+total= total+items[i];
+
+//}
+//System.out.print(total);
+// System.out.println("enter four interger numbers and i will tell you if all is egual or not");
+// System.out.println("enter first number");
+// int first=input.nextInt();
+// 
+// System.out.println("enter first number");
+// int second=input.nextInt();
+// 
+// System.out.println("enter first number");
+// int third=input.nextInt();
+// System.out.println("enter first number");
+// int fouth=input.nextInt();
+// 
+// if (first==second && first==third && first==fouth){
+// System.out.println("EQUAL");
+// 
+// }else{
+// System.out.println("not equal");
+// 
+// }
+//System.out.println("enter first number");
+//double first=input.nextDouble();
+//
+// System.out.println("enter second number");
+//double second=input.nextDouble();
+//
+//if(first>=0 && first<=1 && second>=0 && second<=1){
+//System.out.println("yes the numbers are bet 0 to 1");
+//
+//}else{
+//    
+//System.out.println("false");
+//
+//    }
+//if(second>=0 && second<=1){
+  // System.out.println("yes the numbers are bet 0 to 1");
+
+
+
+    //}else{
+//System.out.println("false");
+       
+   
+    
+   
+    }
+      System.out.println(total);
+
+ 
+}
+}   
+    
+
